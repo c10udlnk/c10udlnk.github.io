8ce0236c4a455547527002e34d23c7a7fc3c686f
cyPh3r <18802682833@163.com>
2021-04-20 01:57:35 +0800

    Site updated: 2021-04-20 01:57:33

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -33,8 +33,8 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "/live2d-api/"
-			// cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+			// apiPath: "/live2d-api/"
+			cdnPath: "https://cdn.jsdelivr.net/gh/c10udlnk/live2d_api@2.3/"
 		});
 	});
 }

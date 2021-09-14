a2c86ce6f18c3cab02849af73b46b6effdbacdd8
cyPh3r <18802682833@163.com>
2021-09-15 01:39:14 +0800

    Site updated: 2021-09-15 01:39:07

--- js/hexo-theme-melody.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const runLightbox = () => {
-    btf.loadLightbox(document.querySelectorAll('#article-container img:not(.no-lightbox)'))
-  }
-}
